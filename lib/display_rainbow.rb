eb491c4cfc33fd864fa150fe1e1ea13311af30cf
Emilia Ziemichód <emifrompoland@gmail.com>
2018-12-23 17:07:45 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -10,6 +10,6 @@ letter=["R","O","Y","G","B","I","V"]
 #puts"#{letter[5]}:#{display_rainbow[5]}"
 #puts"#{letter[6]}:#{display_rainbow[6]}"
 def display_rainbow(colors)
-puts"R: #{colors[0]}"
+puts"R: #{colors[0]}, O: #{colors[1]}, Y:#{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
 display_rainbow(colors)
