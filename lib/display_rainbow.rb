b3f6325a5f13b2614b9ef392394635d4d2640f11
Emilia Ziemichód <emifrompoland@gmail.com>
2018-12-23 16:38:32 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 display_rainbow=["red","orange","yellow","green","blue","indigo","violet"]
 letter=["R","O","Y","G","B","I","V"]
-def display_rainbow
+def display_rainbow(letter)
   #puts"R:#{display_rainbow[0]},O:#{display_rainbow[1]},Y:#{display_rainbow[2]},G:#{display_rainbow[3]},B:#{display_rainbow[4]},I:#{display_rainbow[5]},V:#{display_rainbow[6]}"
 puts"#{letter[0]}:#{display_rainbow[0]}"
 puts"#{letter[1]}:#{display_rainbow[1]}"
@@ -10,4 +10,5 @@ puts"#{letter[4]}:#{display_rainbow[4]}"
 puts"#{letter[5]}:#{display_rainbow[5]}"
 puts"#{letter[6]}:#{display_rainbow[6]}"
 end
-display_rainbow
+display_rainbow(red)
+
