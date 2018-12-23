94f30cdcd9ec9f067eab9a31833cc5c268a3bd1c
Emilia Ziemichód <emifrompoland@gmail.com>
2018-12-23 16:47:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,14 +1,14 @@
-display_rainbow=["red","orange","yellow","green","blue","indigo","violet"]
+colors=["red","orange","yellow","green","blue","indigo","violet"]
 letter=["R","O","Y","G","B","I","V"]
-def display_rainbow(letter)
-  #puts"R:#{display_rainbow[0]},O:#{display_rainbow[1]},Y:#{display_rainbow[2]},G:#{display_rainbow[3]},B:#{display_rainbow[4]},I:#{display_rainbow[5]},V:#{display_rainbow[6]}"
-puts"#{letter[0]}:#{display_rainbow[0]}"
-puts"#{letter[1]}:#{display_rainbow[1]}"
-puts"#{letter[2]}:#{display_rainbow[2]}"
-puts"#{letter[3]}:#{display_rainbow[3]}"
-puts"#{letter[4]}:#{display_rainbow[4]}"
-puts"#{letter[5]}:#{display_rainbow[5]}"
-puts"#{letter[6]}:#{display_rainbow[6]}"
-end
-display_rainbow(red)
 
+  #puts"R:#{display_rainbow[0]},O:#{display_rainbow[1]},Y:#{display_rainbow[2]},G:#{display_rainbow[3]},B:#{display_rainbow[4]},I:#{display_rainbow[5]},V:#{display_rainbow[6]}"
+#puts"#{letter[0]}:#{display_rainbow[0]}"
+#puts"#{letter[1]}:#{display_rainbow[1]}"
+#puts"#{letter[2]}:#{display_rainbow[2]}"
+#puts"#{letter[3]}:#{display_rainbow[3]}"
+#puts"#{letter[4]}:#{display_rainbow[4]}"
+#puts"#{letter[5]}:#{display_rainbow[5]}"
+#puts"#{letter[6]}:#{display_rainbow[6]}"
+def display_rainbow(colors)
+puts"R:#{red}"
+end
