9242307e0691950069e96ddcec5b747c2e23a2be
Emilia Ziemichód <emifrompoland@gmail.com>
2018-12-23 16:26:41 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 display_rainbow=["red","orange","yellow","green","blue","indigo","violet"]
+def display_rainbow
+  puts"R:#{red},O:#{orange},Y:#{yellow},G:#{green},B:#{blue},I:#{indigo},V:#{violet}"
+end
